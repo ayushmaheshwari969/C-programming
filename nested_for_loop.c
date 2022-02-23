929354f25948b9fa1e9fd9eb6cdfcc95ee599e12
ayushmaheshwari969 <97792343+ayushmaheshwari969@users.noreply.github.com>
2022-02-23 16:18:36 +0530

    dgf

---
+++ nested_for_loop.c
@@ -0,0 +1,18 @@
+//-1 , 4 , -7 , 10 , -13.....
+#include<stdio.h>
+
+int main()
+{
+
+for (int i =1 ; i <= 5 ; i++)
+{
+for(int j = 1 ; j<=5 ; j++){
+    printf("%d\n" , i*j);
+};
+printf("i loop end with i = %d\n\n" , i);
+
+
+};
+
+    return 0 ;
+}
